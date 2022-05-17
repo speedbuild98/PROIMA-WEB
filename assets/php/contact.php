5371764f3da0e276149c7b341961ae784a965e80
speedbuild98 <lautaropectm98@gmail.com>
2022-05-17 09:49:30 -0300

    Ni idea que hice

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -12,13 +12,13 @@
         if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
             // Set a 400 (bad request) response code and exit.
             http_response_code(400);
-            echo "Please complete the form and try again.";
+            echo "Por favor completa los campos e intenta otra vez.";
             exit;
         }
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "email@gmail.com";
+        $recipient = "lautaropectm98@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
@@ -35,17 +35,17 @@
         if (mail($recipient, $email_headers, $email_content)) {
             // Set a 200 (okay) response code.
             http_response_code(200);
-            echo "Thank You! Your message has been sent.";
+            echo "¡Gracias! El mensaje fue enviado.";
         } else {
             // Set a 500 (internal server error) response code.
             http_response_code(500);
-            echo "Oops! Something went wrong and we couldn't send your message.";
+            echo "¡Oops! Algo salió mal y no se pudo enviar el mensaje.";
         }
 
     } else {
         // Not a POST request, set a 403 (forbidden) response code.
         http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
+        echo "Hay un problema con tu mensaje, intentá de nuevo.";
     }
 
 ?>
